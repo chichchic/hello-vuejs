be63ed4fc5af5433d00a0d8fda56119760d1b80e
chichchic <pch6789@naver.com>
2022-01-24 11:11:40 +0900

    feat: add global navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
+import nullGuard from '@/middlewares/nullGuard'
 
 Vue.use(VueRouter)
 
 const router = Promise.all(routes).then(routes => {
-  return new VueRouter({
+  const vRouter = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes
   })
+  vRouter.beforeEach((to, from, next) => {
+    nullGuard({to, from}, next, next.bind(null, {name: 'DateTime'}))
+  })
+  return vRouter
 })
 
 export default router
