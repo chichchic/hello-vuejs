8550449f0ee997b2878c334b55a542cee638859e
chichchic <pch6789@naver.com>
2022-01-27 17:48:46 +0900

    reafctor: vi18n config 자동화 코드

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import VueI18n from "vue-i18n";
+
+import { extractFiles, keyFileReducer } from './util';
 Vue.use(VueI18n);
 const languages = {
   ko: "한국어",
@@ -22,22 +24,12 @@ const locales = [
 ];
 Vue.prototype.$locales = locales;
 Vue.prototype.$languages = languages;
-const messagesPromises = Object.keys(languages).map(async ( lang ) => {
-  const { default: file } = await import(`@/locales/messages/${lang}`);
-  return { key: lang, file };
-});
-const dateTimeFormatsPromises = locales.map(async ({ locale }) => {
-  const { default: file } = await import(`@/locales/dateTimeFormats/${locale}`);
-  return { key: locale, file };
-});
-const pluralPromises = Object.keys(languages).map(async ( lang ) => {
-  const { default: file } = await import(`@/locales/pluralizationRules/${lang}`);
-  return { key: lang, file };
-});
-const numberFormatPromises = locales.map(async ( { locale } ) => {
-  const { default: file } = await import(`@/locales/numberFormats/${locale}`);
-  return { key: locale, file };
-});
+
+const messagesPromises = extractFiles(Object.keys(languages), 'messages');
+const dateTimeFormatsPromises = extractFiles(locales.map(v => v.locale), 'dateTimeFormats');
+const pluralPromises = extractFiles(Object.keys(languages), 'pluralizationRules');
+const numberFormatPromises = extractFiles(locales.map(v => v.locale), 'numberFormats');
+
 const i18nConfig = [
   Promise.all(dateTimeFormatsPromises),
   Promise.all(messagesPromises),
@@ -49,22 +41,10 @@ const i18n = Promise.all(i18nConfig).then((res) => {
   return new VueI18n({
     locale: "ko",
     fallbackLocale: "en",
-    messages: messages.reduce(
-      (acc, { key, file }) => ({ ...acc, [key]: file }),
-      {}
-    ),
-    dateTimeFormats: dateTimeFormats.reduce(
-      (acc, { key, file }) => ({ ...acc, [key]: file }),
-      {}
-    ),
-    pluralizationRules: pluralizationRules.reduce(
-      (acc, { key, file }) => ({ ...acc, [key]: file }),
-      {}
-    ),
-    numberFormats: numberFormats.reduce(
-      (acc, { key, file }) => ({ ...acc, [key]: file }),
-      {}
-    ),
+    messages: keyFileReducer(messages),
+    dateTimeFormats: keyFileReducer(dateTimeFormats),
+    pluralizationRules: keyFileReducer(pluralizationRules),
+    numberFormats: keyFileReducer(numberFormats)
   });
 });
 
---
+++ src/locales/util.js
@@ -0,0 +1,15 @@
+function extractFiles(fileNames, folderName) {
+  return fileNames.map(async ( fileName ) => {
+    const { default: file } = await import(`@/locales/${folderName}/${fileName}`);
+    return { key: fileName, file };
+  });
+}
+
+function keyFileReducer(fileObjects) {
+  return fileObjects.reduce(
+    (acc, { key, file }) => ({ ...acc, [key]: file }),
+    {}
+  )
+}
+
+export { extractFiles, keyFileReducer };
