ad83ba12291c1703ecbfb68c4f9a882b0499d404
chichchic <pch6789@naver.com>
2022-01-27 11:22:59 +0900

    feat: remove unuseful HTML tag

--- src/views/DateTime.vue
+++ src/views/DateTime.vue
@@ -1,37 +1,35 @@
 <template>
-  <section class="i18n">
-    <section class="localization">
-      <h1>localization</h1>
-      <section class="control">
-        <button
-          :class="{ selected: locale === curLocale }"
-          v-for="{ locale, localeName } in $locales"
-          @click="changeLocale(locale)"
-        >
-          {{ localeName }}
-        </button>
-      </section>
-      <p>
-        Date: <span>{{ $d(new Date(), "Date", curLocale) }} </span>
-      </p>
-      <p>
-        Time:
-        <span :class="{ rtl: isArabic }" :dir="attrDir"
-          >{{ $d(new Date(), "Time", curLocale) }}
-        </span>
-      </p>
-      <p class="date-time">
-        DateTime:
-        <span :class="{ arabic: isArabic }">
-          <span :dir="attrDir">
-            {{ $d(new Date(), "Date", curLocale) }}
-          </span>
-          <span v-if="!isArabic">,&nbsp;</span>
-          <span v-else>&nbsp;</span>
-          <span :dir="attrDir">{{ $d(new Date(), "Time", curLocale) }}</span>
-        </span>
-      </p>
+  <section class="date-time">
+    <h1>DateTime</h1>
+    <section class="control">
+      <button
+        :class="{ selected: locale === curLocale }"
+        v-for="{ locale, localeName } in $locales"
+        @click="changeLocale(locale)"
+      >
+        {{ localeName }}
+      </button>
     </section>
+    <p>
+      Date: <span>{{ $d(new Date(), "Date", curLocale) }} </span>
+    </p>
+    <p>
+      Time:
+      <span :class="{ rtl: isArabic }" :dir="attrDir"
+        >{{ $d(new Date(), "Time", curLocale) }}
+      </span>
+    </p>
+    <p class="date-time">
+      DateTime:
+      <span :class="{ arabic: isArabic }">
+        <span :dir="attrDir">
+          {{ $d(new Date(), "Date", curLocale) }}
+        </span>
+        <span v-if="!isArabic">,&nbsp;</span>
+        <span v-else>&nbsp;</span>
+        <span :dir="attrDir">{{ $d(new Date(), "Time", curLocale) }}</span>
+      </span>
+    </p>
   </section>
 </template>
 <script>
@@ -57,7 +55,7 @@ export default {
 </script>
 <style lang="scss" scoped>
 .i18n {
-  .localization {
+  .dateTime {
     p {
       display: flex;
       width: 40rem;
