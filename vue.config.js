a79fd472ccf7b998d34a23eff9b074e738deb54f
chichchic <pch6789@naver.com>
2022-02-18 16:59:55 +0900

    일시저장

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,81 @@
+const rtlcss = require("rtlcss");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        prependData: `@import "@/assets/scss/index.scss";`
-      }
-    }
+        prependData: `@import "@/assets/scss/index.scss";`,
+      },
+    },
+    // extract: {
+    //   filename: "css/[name].[hash].css",
+    //   chunkFilename: "css/[name].[hash].css",
+    // },
   },
-  productionSourceMap: true,
-  chainWebpack: config => {
-    config.module.rules.delete('eslint');
-  }
-}
+  chainWebpack: (config) => {
+    config.module.rules.delete("eslint");
+    config.plugin("css").use(MiniCssExtractPlugin, [
+      {
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      },
+    ]);
+    // config.module
+    //   .rule("scss")
+    //   .test(/RTLCSSExamples.vue/)
+    //   .oneOf("vue")
+    //   .use("vue-style")
+    //   .loader("vue-style-loader")
+    //   .end()
+    //   .end()
+    //   .oneOf("normal")
+    //   .use("sass")
+    //   .loader("sass-loader")
+    //   .tap(() => ({
+    //     prependData: `@import "@/assets/scss/index.scss";`,
+    //   }))
+    //   .end()
+    //   .end()
+    //   .after("vue")
+    //   .use("postcss-loader")
+    //   .tap(() => ({
+    //     postcssOptions: {
+    //       plugins: [rtlcss({})],
+    //     },
+    //   }));
+    config.module
+      .rule("scss")
+      .oneOf("vue")
+      .test(/RTLCSSExamples/)
+
+      .use("postcss-loader")
+      .tap(() => ({
+        sourceMap: true,
+        postcssOptions: {
+          plugins: [
+            rtlcss({
+              stringMap: [
+                {
+                  name: "ltr-rtl",
+                  priority: 100,
+                  search: "ltr-",
+                  replace: "rtl-",
+                  options: {
+                    ignoreCase: false,
+                    greedy: true,
+                  },
+                },
+              ],
+            }),
+          ],
+        },
+      }))
+      .end()
+      .before("sass-loader")
+      .use("vue-style-loader")
+      .end()
+      .oneOf("css")
+      .use("mini-extract-css-loader")
+      .loader(MiniCssExtractPlugin.loader);
+  },
+};
